b61697b2ee0263ac31db886d474681c13d9a48b7
Javier-Azahel-Gutierrez <a219209037@unison.mx>
2023-09-27 14:33:46 -0700

    PDF

--- db/query-maestros.php
+++ db/query-maestros.php
@@ -4,31 +4,35 @@ $conn = (new Common)->connection();
 //Trabajito de Gabi, Luisa y Sebitas
 $error = 1;
 $idMaestro = $_POST['IdMaestro'];
-if($conn->connect_errno){
+if ($conn->connect_errno) {
     $error = 1;
-}else{$sql = "select * from tiempo_completo where num_empleado = '" . $idMaestro."'";
-$rows1 = mysqli_fetch_row($conn->query($sql));
-
-
-
-if ($rows1 === null) {
-    $sql = "select * from tecnico_academico where num_empleado = '" . $idMaestro."'";
+} else {
+    $sql = "select * from tiempo_completo where num_empleado = '" . $idMaestro . "'";
     $rows1 = mysqli_fetch_row($conn->query($sql));
+
+
+
     if ($rows1 === null) {
-        $sql = "select * from asignatura where num_empleado = '" . $idMaestro."'";
+        $sql = "select * from tecnico_academico where num_empleado = '" . $idMaestro . "'";
         $rows1 = mysqli_fetch_row($conn->query($sql));
         if ($rows1 === null) {
-            $error = 3;
-            
+            $sql = "select * from asignatura where num_empleado = '" . $idMaestro . "'";
+            $rows1 = mysqli_fetch_row($conn->query($sql));
+            if ($rows1 === null) {
+                $error = 3;
+
+            } else {
+                array_push($rows1, "asignatura");
+                $error = $rows1;
+            }
         } else {
+            array_push($rows1, "tecnico_academico");
             $error = $rows1;
         }
     } else {
+        array_push($rows1, "tiempo_completo");
         $error = $rows1;
     }
-} else {
-    $error = $rows1;
-}
 }
 
 echo json_encode($error);
--- js/gen-pdf-maestro.js
+++ js/gen-pdf-maestro.js
@@ -1,6 +1,42 @@
+import { convertType } from "./common/common.js";
 $(document).ready(function () {
     $("#buscar").click(function () {
-        
+        let idMaestro = $("#id-prof").val();
+        if (idMaestro !== "" || +idMaestro !== NaN) {
+            $.post("db/query-maestros.php", { IdMaestro: idMaestro }, function (data) {
+                let result = JSON.parse(data);
+                console.log(result);
+                $("#r_maestro").text(result[1]);
+                $("#r_tipo-maestro").text(convertType(result[result.length - 1]));
+            });
+        } else {
+            alert("Ingrese un numero de empleado valido");
+        }
+    });
+
+    $("#btn-gen-pdf").click(async function () {
+        window.jsPDF = window.jspdf.jsPDF;
+        window.html2canvas = html2canvas;
+
+        let doc = new jsPDF();
+        let elementHTML = await $('#content').html();
+
+        doc.html(elementHTML, {
+            callback: function () {
+                $("#embed-pdf").attr("src", URL.createObjectURL(doc.output("blob")));
+            },
+            margin: [10, 10, 10, 10],
+            autoPaging: 'text',
+            x: 0,
+            y: 0,
+            width: 190, //target width in the PDF document
+            windowWidth: 675 //window width in CSS pixels
+        },
+        );
     });
 }
 );
+
+/**
+ * https://www.codexworld.com/convert-html-to-pdf-using-javascript-jspdf/
+ */
