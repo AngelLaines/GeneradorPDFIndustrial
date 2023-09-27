4a1fe472ca50df461a7828001a795c71137a6d6a
Javier-Azahel-Gutierrez <a219209037@unison.mx>
2023-09-27 15:39:37 -0700

    pdf

--- js/gen-pdf-maestro.js
+++ js/gen-pdf-maestro.js
@@ -1,4 +1,5 @@
 import { convertType } from "./common/common.js";
+import { tiempoCompleto } from "./common/json-teachers.js";
 $(document).ready(function () {
     $("#buscar").click(function () {
         let idMaestro = $("#id-prof").val();
@@ -6,6 +7,19 @@ $(document).ready(function () {
             $.post("db/query-maestros.php", { IdMaestro: idMaestro }, function (data) {
                 let result = JSON.parse(data);
                 console.log(result);
+                let listDiv = $("#list");
+                
+                    if (result[result.length-1]==="tiempo_completo") {
+                        listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.A[result[2]]+"</p>");
+                        listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.B[result[3]]+"</p>");
+                        listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.C[result[4]]+"</p>");
+                        listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.D[result[5]]+"</p>");
+                        if (result[6]!=="") {
+                            listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.E[result[6]]+"</p>");
+                        }
+                        listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.F[result[7]]+"</p>");
+                    }
+            
                 $("#r_maestro").text(result[1]);
                 $("#r_tipo-maestro").text(convertType(result[result.length - 1]));
             });
@@ -18,19 +32,22 @@ $(document).ready(function () {
         window.jsPDF = window.jspdf.jsPDF;
         window.html2canvas = html2canvas;
 
-        let doc = new jsPDF();
-        let elementHTML = await $('#content').html();
+        let docu = new jsPDF();
+        let elementHTML = document.querySelector("#content");
 
-        doc.html(elementHTML, {
-            callback: function () {
-                $("#embed-pdf").attr("src", URL.createObjectURL(doc.output("blob")));
-            },
-            margin: [10, 10, 10, 10],
+       
+        docu.html(elementHTML, {
+            margin: [20, 20, 20, 20],
             autoPaging: 'text',
             x: 0,
             y: 0,
-            width: 190, //target width in the PDF document
-            windowWidth: 675 //window width in CSS pixels
+            width: 250, //target width in the PDF document
+            windowWidth: 675, //window width in CSS pixels
+            callback: function (doc) {
+                window.open(URL.createObjectURL(doc.output("blob")));
+                $("#embed-pdf").attr("src", URL.createObjectURL(doc.output("blob")));
+            },
+            
         },
         );
     });
