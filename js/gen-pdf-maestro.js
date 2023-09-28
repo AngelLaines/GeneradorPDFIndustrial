5ec51ca07799eff930d89d49c3bfc63e49a675f0
AngelLaines <angelmoreno755@gmail.com>
2023-09-28 10:59:23 -0700

    PDDF

--- js/gen-pdf-maestro.js
+++ js/gen-pdf-maestro.js
@@ -8,18 +8,18 @@ $(document).ready(function () {
                 let result = JSON.parse(data);
                 console.log(result);
                 let listDiv = $("#list");
-                
-                    if (result[result.length-1]==="tiempo_completo") {
-                        listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.A[result[2]]+"</p>");
-                        listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.B[result[3]]+"</p>");
-                        listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.C[result[4]]+"</p>");
-                        listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.D[result[5]]+"</p>");
-                        if (result[6]!=="") {
-                            listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.E[result[6]]+"</p>");
-                        }
-                        listDiv.append("<p style='font-size:10px;'>"+tiempoCompleto.F[result[7]]+"</p>");
+                listDiv.empty();
+                if (result[result.length - 1] === "tiempo_completo") {
+                    listDiv.append("<p style='font-size:16px;'>" + tiempoCompleto.A[result[2]] + "</p>");
+                    listDiv.append("<p style='font-size:16px;'>" + tiempoCompleto.B[result[3]] + "</p>");
+                    listDiv.append("<p style='font-size:16px;'>" + tiempoCompleto.C[result[4]] + "</p>");
+                    listDiv.append("<p style='font-size:16px;'>" + tiempoCompleto.D[result[5]] + "</p>");
+                    if (result[6] !== "") {
+                        listDiv.append("<p style='font-size:16px;'>" + tiempoCompleto.E[result[6]] + "</p>");
                     }
-            
+                    listDiv.append("<p style='font-size:16px;'>" + tiempoCompleto.F[result[7]] + "</p>");
+                }
+
                 $("#r_maestro").text(result[1]);
                 $("#r_tipo-maestro").text(convertType(result[result.length - 1]));
             });
@@ -28,32 +28,18 @@ $(document).ready(function () {
         }
     });
 
-    $("#btn-gen-pdf").click(async function () {
-        window.jsPDF = window.jspdf.jsPDF;
-        window.html2canvas = html2canvas;
+    $("#btn-gen-pdf").click(function (e) {
 
-        let docu = new jsPDF();
-        let elementHTML = document.querySelector("#content");
-
-       
-        docu.html(elementHTML, {
-            margin: [20, 20, 20, 20],
-            autoPaging: 'text',
-            x: 0,
-            y: 0,
-            width: 250, //target width in the PDF document
-            windowWidth: 675, //window width in CSS pixels
-            callback: function (doc) {
-                window.open(URL.createObjectURL(doc.output("blob")));
-                $("#embed-pdf").attr("src", URL.createObjectURL(doc.output("blob")));
-            },
-            
-        },
-        );
+        // e.preventDefault();
+        // let content = $("#content");
+        // let uri = 'test.pdf#toolbar=0&navpanes=0&scrollbar=0';
+        const mode = 'iframe';
+        const close = mode==='iframe';
+        const options = {
+            mode,popClose:close
+        }
+        $("#content").printArea(options);
+        
+        
     });
-}
-);
-
-/**
- * https://www.codexworld.com/convert-html-to-pdf-using-javascript-jspdf/
- */
+});
